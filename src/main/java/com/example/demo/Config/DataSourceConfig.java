3fbc4318ce9ae4bfe4954ea40af5f481c39f350d
Euzeth <knightcy1@gmail.com>
2023-10-17 19:16:08 +0900

    로그인은 가능 그외는 작업중

--- src/main/java/com/example/demo/Config/DataSourceConfig.java
+++ src/main/java/com/example/demo/Config/DataSourceConfig.java
@@ -1,12 +1,14 @@
 package com.example.demo.Config;
 
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariDataSource;
@@ -40,11 +42,16 @@ public class DataSourceConfig {
 		dataSource.setPassword("1234");	 
 		return dataSource;
 	}
-	
-    @Bean
-    public DataSourceTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
-	
+
+	private HikariDataSource dataSource;
+
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		transactionManager.setDataSource(dataSource);
+		return transactionManager;
+	}
 	
 }
